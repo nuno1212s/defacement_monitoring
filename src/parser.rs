aba056f96da3ec197ae98c8eb89df184608238f1
Nuno neto <nuno.neto.g@gmail.com>
2021-12-15 14:56:10 +0000

    Initial things for reading webpages into rust.

---
+++ src/parser.rs
@@ -0,0 +1,25 @@
+use std::process::Command;
+
+const CHROME_HEADLESS: String = String::from("chrome");
+const HEADLESS: String = String::from("--headless");
+const DUMP_TO_DOM: String = String::from("--dump-dom");
+
+/**
+Runs chromium headless to render and then obtain the websites we want
+This allows our program to get the full website after
+*/
+pub fn read_website_to_dom(website: &str) -> String {
+    let command = Command::new(CHROME_HEADLESS)
+        .arg(HEADLESS)
+        .arg(DUMP_TO_DOM)
+        .arg(website);
+
+    let result = command.output()
+        .expect("Failed to execute chrome to fetch website");
+
+    String::from_utf8(result.stdout).expect("Failed to parse return text")
+}
+
+pub fn read_website_to_pdf(website: &str) {
+
+}
